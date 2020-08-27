9e0bd5cc66bcd589d9f9655d7e3bd6717032923b
rinvas <vasilyman@mail.ru>
2020-08-27 22:29:48 +0500

    added build

---
+++ dist/js/filters.chunks.js
@@ -0,0 +1,63 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["filters"],{
+
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/components/IndexFilters.vue?vue&type=script&lang=js&":
+/*!************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--1!./node_modules/vue-loader/lib??vue-loader-options!./src/components/IndexFilters.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  data: function data() {\n    return {\n      content: 'ffff'\n    };\n  },\n  mounted: function mounted() {\n    console.log('mounted');\n    this.$root.$emit('remove', 'emitted from filters');\n  }\n});\n\n//# sourceURL=webpack:///./src/components/IndexFilters.vue?./node_modules/babel-loader/lib??ref--1!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e&":
+/*!******************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e& ***!
+  \******************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _vm._m(0)\n}\nvar staticRenderFns = [\n  function() {\n    var _vm = this\n    var _h = _vm.$createElement\n    var _c = _vm._self._c || _h\n    return _c(\"div\", { staticClass: \"card border-0 sidebar sticky-bar\" }, [\n      _c(\"div\", { staticClass: \"card-body p-0\" }, [\n        _c(\"div\", { staticClass: \"widget mt-4 pt-2\" }, [\n          _c(\"h4\", { staticClass: \"widget-title\" }, [_vm._v(\"Фильтры\")]),\n          _vm._v(\" \"),\n          _c(\"ul\", { staticClass: \"list-unstyled mt-4 mb-0 blog-catagories\" }, [\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [_vm._v(\"Men\")])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Women\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Electronics\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Jewellery\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Shoes\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Kid’s Wear\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Sports\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Toys\")\n              ])\n            ]),\n            _vm._v(\" \"),\n            _c(\"li\", [\n              _c(\"a\", { attrs: { href: \"jvascript:void(0)\" } }, [\n                _vm._v(\"Gift Corners\")\n              ])\n            ])\n          ])\n        ])\n      ])\n    ])\n  }\n]\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/components/IndexFilters.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/components/IndexFilters.vue":
+/*!*****************************************!*\
+  !*** ./src/components/IndexFilters.vue ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./IndexFilters.vue?vue&type=template&id=d0622b0e& */ \"./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e&\");\n/* harmony import */ var _IndexFilters_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./IndexFilters.vue?vue&type=script&lang=js& */ \"./src/components/IndexFilters.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"./node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n\nvar component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _IndexFilters_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__[\"render\"],\n  _IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"],\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/components/IndexFilters.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);\n\n//# sourceURL=webpack:///./src/components/IndexFilters.vue?");
+
+/***/ }),
+
+/***/ "./src/components/IndexFilters.vue?vue&type=script&lang=js&":
+/*!******************************************************************!*\
+  !*** ./src/components/IndexFilters.vue?vue&type=script&lang=js& ***!
+  \******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_1_node_modules_vue_loader_lib_index_js_vue_loader_options_IndexFilters_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/babel-loader/lib??ref--1!../../node_modules/vue-loader/lib??vue-loader-options!./IndexFilters.vue?vue&type=script&lang=js& */ \"./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/components/IndexFilters.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__[\"default\"] = (_node_modules_babel_loader_lib_index_js_ref_1_node_modules_vue_loader_lib_index_js_vue_loader_options_IndexFilters_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack:///./src/components/IndexFilters.vue?");
+
+/***/ }),
+
+/***/ "./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e&":
+/*!************************************************************************!*\
+  !*** ./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e& ***!
+  \************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../node_modules/vue-loader/lib??vue-loader-options!./IndexFilters.vue?vue&type=template&id=d0622b0e& */ \"./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/IndexFilters.vue?vue&type=template&id=d0622b0e&\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_IndexFilters_vue_vue_type_template_id_d0622b0e___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"]; });\n\n\n\n//# sourceURL=webpack:///./src/components/IndexFilters.vue?");
+
+/***/ })
+
+}]);
